3c48e5a9c84258dfbb4f4e5727b1bd0d8bb23c11
jigspatel_it <jigspatel.04@gmail.com>
2019-12-06 15:03:46 +0530

    updating our app to redirect to home if user is signed in

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,8 +5,8 @@ import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import "./header.scss";
 import { auth } from "../../firebase/firebase.utils";
+import "./header.scss";
 
 const Header = ({ currentUser }) => (
   <div className="header">
