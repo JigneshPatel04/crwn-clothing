90d1083dbafcc6f0142000b7036e6cb44e401288
jigspatel_it <jigspatel.04@gmail.com>
2020-04-22 10:10:21 +0530

    updating stripe intrigation and update tile and favicon

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 import {
   selectCartItems,
-  selectCartTotal
+  selectCartTotal,
 } from '../../redux/cart/cart.selectors';
 
 import './checkout.scss';
@@ -32,18 +33,24 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => (
+    {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>
       <span>TOTAL : ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 - Exp:01/21 - CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
